65a976e93688ee9f91242affd0f82700ad5c38f3
devseag <noexample@gmail.com>
2023-09-16 10:54:26 -0300

    add components employees
    6 126

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Track employees in company N</h1>
+            <h2>All number of employees: </h2>
+            <h2>Benefit is received:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
