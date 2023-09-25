e097f1d0d51ce35e0fef88310f62764caa39e710
devseag <noexample@gmail.com>
2023-09-25 17:47:55 -0300

    add use context function
    8 178

---
+++ src/Input.js
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import dataContext from './context';
+
+const InputComponent = () => {
+
+    const context = useContext(dataContext);
+    console.log(context.forceChangeMail); // undefined
+
+    return (
+            <input 
+                value={context.mail} 
+                type="email" 
+                className='form-control' 
+                placeholder="name@example.com"
+                onFocus={context.forceChangeMail}/>
+    )
+}
+
+export default InputComponent;
