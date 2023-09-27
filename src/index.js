509068d5c0d6dcc08fd79dce624c5b7691d6b284
devseag <noexample@gmail.com>
2023-09-27 10:24:38 -0300

    add counter react
    9 193

--- src/index.js
+++ src/index.js
@@ -5,76 +5,34 @@ import reducer from './reducer';
 // import {inc, dec, rnd} from './actions';
 import * as actions from './actions';
 
+import Counter from './Counter';
+
 
 const store = createStore(reducer);
 
 const {dispatch, subscribe, getState} = store;
 
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
-
-// const incDispatch = () =>  dispatch(inc());
-// const decDispatch = () =>  dispatch(dec());
-// const rndDispatch = (value) =>  dispatch(rnd(value));
-// const incDispatch = bindActionCreator(inc, dispatch);
-// const decDispatch = bindActionCreator(dec, dispatch);
-// const rndDispatch = bindActionCreator(rnd, dispatch);
-
-// const incDispatch = bindActionCreators(inc, dispatch);
-// const decDispatch = bindActionCreators(dec, dispatch);
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-// const {incDispatch, decDispatch, rndDispatch }= bindActionCreators({
-//     incDispatch: inc,
-//     decDispatch: dec,
-//     rndDispatch: rnd
-// }, dispatch);
-
-// const {incDispatch, decDispatch, rndDispatch }= bindActionCreators({
-//     inc: inc,
-//     dec: dec,
-//     rnd: rnd
-// }, dispatch);
-
-// const {incDispatch, decDispatch, rndDispatch }= bindActionCreators({
-//     inc,
-//     dec,
-//     rnd
-// }, dispatch);
-
 const {inc, dec, rnd}= bindActionCreators(actions, dispatch);
 
-// document.getElementById('inc').addEventListener('click', incDispatch);
+const update = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <>
+        <Counter 
+          counter={getState().value}
+          inc={inc}
+          dec={dec}
+          rnd={() =>{
+              const value = Math.floor(Math.random() * 10);
+              rnd(value);
+          }}
+        />
+      </>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
-// document.getElementById('dec').addEventListener('click', decDispatch);
+update();
 
-// document.getElementById('rnd').addEventListener('click', () => {
-//     const value = Math.floor(Math.random() * 10);
-//     rndDispatch(value);
-// });
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-});
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <>
-    
-    </>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+subscribe(update);
